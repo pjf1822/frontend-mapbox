9a2fbbaf66fb570359d26ece36bdd230110c45d2
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-08-15 20:11:50 -0400

    env implemented

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,7 @@ import tattooLogo from "./assets/output-onlinepngtools.png";
 import { createAddress, deleteAddress, fetchAddresses } from "./api";
 import PopupContent from "./components/PopupContent";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoicGpmMTgyMiIsImEiOiJjbGZybHJsMXMwMmd3M3BwMmFiZXlvZjczIn0.68xXIxxj_-iONU42ihPWZA";
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
 export default function App() {
   const [listOfAddresses, setListOfAddresses] = useState([]);
@@ -75,7 +74,7 @@ export default function App() {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/pjf1822/clekajgr3000001l8y22r3psx",
+      style: process.env.REACT_APP_MAPBOX_STYLE_KEY,
       center: [lng, lat],
       zoom: zoom,
     });
